69f14e6b0281a488172748e8b627fe7821994638
Sunil Sharma <sunil2342@gmail.com>
2020-12-14 18:26:08 +0530

    removed api keys

--- ui/dev/src/boot/register.js
+++ ui/dev/src/boot/register.js
@@ -3,10 +3,10 @@ import QMaps from "@sourcesync/quasar-ui-maps"; // "@sourcesync/quasar-ui-maps" 
 
 // Vue.use(QMaps, {
 //   mode: "gmaps",
-//   accessToken: "pk.eyJ1Ijoic29mdHJhdyIsImEiOiJjazZzdTR4OW4wajF4M29udnZxMGxseXozIn0.YoK-b6rkj9Nw0zkWzvD0Xg"
+//   accessToken: ""
 // });
 
 Vue.use(QMaps, {
   mode: "gmaps",
-  accessToken: "AIzaSyBSSY_LeMoxxG0Re23ZRw7vd3PdwCSt4qI"
+  accessToken: ""
 });
